1a666dde5c0d118a7750a7dd577eff4a704cf24b
nboaram <nboaram1@hotmail.co.uk>
2019-07-08 16:11:30 +0100

    edited shell

--- MeanSetup.sh
+++ MeanSetup.sh
@@ -1,4 +1,5 @@
 !#/bin/bash
+cd
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
@@ -9,7 +10,7 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 npm init -y
 npm install --save mongoose
-npm install --save ex
+npm install --save express
 yes | sudo npm install -g @angular/cli
 npm install --save-dev @angular-devkit/build-angular
 npm install --save bootstrap
@@ -26,4 +27,3 @@ git checkout Developer
 sudo systemctl daemon-reload
 sudo systemctl enable angular
 sudo systemctl start angular
-ng serve
