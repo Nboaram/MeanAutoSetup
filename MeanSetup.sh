bb46bf67e1a1413e5f40ceb37cbde9daaad47e0c
nboaram <nboaram1@hotmail.co.uk>
2019-07-08 17:21:34 +0100

    repositioned git clones

--- MeanSetup.sh
+++ MeanSetup.sh
@@ -1,4 +1,7 @@
 !#/bin/bash
+cd
+git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
+git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
@@ -18,7 +21,7 @@ sudo npm install --save bootstrap
 sudo npm install --save cors
 sudo cp angular.service /etc/systemd/system/angular.service
 sudo cp api.service /etc/systemd/system/api.service
-cd /TeamAPoolProjectBackend
+cd ~/TeamAPoolProjectBackend
 git checkout Developer 
 sudo systemctl daemon-reload
 sudo systemctl enable api
