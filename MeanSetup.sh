7a7523ea84f894cc796915d396417da59b3933c0
nboaram <nboaram1@hotmail.co.uk>
2019-07-08 14:22:32 +0100

    Initial Files

---
+++ MeanSetup.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+npm init -y
+npm install --save mongoose
+npm install --save ex
+yes | sudo npm install -g @angular/cli
+npm install --save-dev @angular-devkit/build-angular
+npm install --save bootstrap
+npm install --save cors
+sudo cp angular.service /etc/systemd/system/angular.service
+cd /TeamAPoolProjectBackend
+cd /TeamAPoolProjectUI
+git checkout Developer
+ng serve
