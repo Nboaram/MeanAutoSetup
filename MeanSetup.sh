0e0f92d3bd7f17ebedbe12c5d0947b3f659f72ae
nboaram <nboaram1@hotmail.co.uk>
2019-07-08 16:32:34 +0100

    edited shell to include sudo

--- MeanSetup.sh
+++ MeanSetup.sh
@@ -1,5 +1,4 @@
 !#/bin/bash
-cd
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
@@ -8,13 +7,15 @@ sudo service mongod start
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
-npm init -y
-npm install --save mongoose
-npm install --save express
+sudo npm init -y
+sudo npm install --save mongoose
+sudo npm install --save express
 yes | sudo npm install -g @angular/cli
-npm install --save-dev @angular-devkit/build-angular
-npm install --save bootstrap
-npm install --save cors
+sudo npm install --save-dev @angular-devkit/build-angular
+sudo npm install --save jquery
+sudo npm install --save popper.
+sudo npm install --save bootstrap
+sudo npm install --save cors
 sudo cp angular.service /etc/systemd/system/angular.service
 sudo cp api.service /etc/systemd/system/api.service
 cd /TeamAPoolProjectBackend
