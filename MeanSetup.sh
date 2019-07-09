370b558b053f42b219f719a3d1ab781ff493ca90
nboaram <nboaram1@hotmail.co.uk>
2019-07-09 09:56:17 +0100

    changed services and added echo's

--- MeanSetup.sh
+++ MeanSetup.sh
@@ -1,6 +1,16 @@
 !#/bin/bash
 cd
+echo ////////////////////////////////////////////////////////////////
+echo
+echo            Git Cloning Backend
+echo
+echo ////////////////////////////////////////////////////////////////
 sudo git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
+echo ////////////////////////////////////////////////////////////////
+echo
+echo            Git Cloning Frontend
+echo
+echo ////////////////////////////////////////////////////////////////
 sudo git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
